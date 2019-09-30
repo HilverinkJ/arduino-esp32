248a3e104b80ba37cc5a4d9fdf87472b7be18d95
me-no-dev <hristo@espressif.com>
2019-10-01 01:30:12 +0300

    Update create-package-json.sh

--- tools/ci/create-package-json.sh
+++ tools/ci/create-package-json.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-function downloadAndMergePackageJSON(){
+function merge_package_json(){
 	local jsonLink=$1
 	local jsonOut=$2
 	local old_json=$OUTPUT_DIR/oldJson.json
@@ -28,8 +28,11 @@ function downloadAndMergePackageJSON(){
 	set +v
 }
 
+PACKAGE_JSON_DEV="package_esp32_dev_index.json"
+PACKAGE_JSON_REL="package_esp32_index.json"
+
 echo "Getting previous releases ..."
-releasesJson=`curl -sH "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/$GITHUB_REPOSITORY/releases`
+releasesJson=`curl -sH "Authorization: token $GITHUB_TOKEN" "https://api.github.com/repos/$GITHUB_REPOSITORY/releases"`
 if [ $? -ne 0 ]; then echo "ERROR: Get Releases Failed! ($?)"; exit 1; fi
 
 prev_release=$(echo "$releasesJson" | jq -e -r '. | map(select(.draft == false and .prerelease == false)) | sort_by(.created_at | - fromdateiso8601) | .[0].tag_name')
@@ -39,6 +42,7 @@ if [ "$prev_any_release" == "$RELEASE_TAG" ]; then
 	prev_release=$(echo "$releasesJson" | jq -e -r '. | map(select(.draft == false and .prerelease == false)) | sort_by(.created_at | - fromdateiso8601) | .[1].tag_name')
 	prev_any_release=$(echo "$releasesJson" | jq -e -r '. | map(select(.draft == false)) | sort_by(.created_at | - fromdateiso8601)  | .[1].tag_name')
 fi
+commits_release="$prev_any_release"
 shopt -u nocasematch
 
 echo "Previous Release: $prev_release"
@@ -53,15 +57,11 @@ jq_arg=".packages[0].platforms[0].version = \"$RELEASE_TAG\" | \
 	.packages[0].platforms[0].size = \"$PKG_SIZE\" |\
 	.packages[0].platforms[0].checksum = \"SHA-256:$PKG_SHA\""
  
-
-PACKAGE_JSON_DEV="package_esp32_dev_index.json"
-PACKAGE_JSON_REL="package_esp32_index.json"
-
 # always get DEV version of JSON (included in both RC/REL)
 echo "Genarating $PACKAGE_JSON_DEV ..."
 cat "$GITHUB_WORKSPACE/package/package_esp32_index.template.json" | jq "$jq_arg" > "$OUTPUT_DIR/$PACKAGE_JSON_DEV"
 if [ ! -z "$prev_any_release" ] && [ "$prev_any_release" != "null" ]; then
-	downloadAndMergePackageJSON "$prev_any_release/$PACKAGE_JSON_DEV" "$OUTPUT_DIR/$PACKAGE_JSON_DEV"
+	merge_package_json "$prev_any_release/$PACKAGE_JSON_DEV" "$OUTPUT_DIR/$PACKAGE_JSON_DEV"
 	echo "Getting commits from $prev_any_release ..."
 	git -C "$GITHUB_WORKSPACE" log --oneline $prev_any_release.. > "$OUTPUT_DIR/commits.txt"
 fi
@@ -70,12 +70,11 @@ echo "Download URL: "`git_safe_upload_asset "$OUTPUT_DIR/$PACKAGE_JSON_DEV"`
 
 # for RELEASE run update REL JSON as well
 if [ "$RELEASE_PRE" == "false" ]; then
+	commits_release="$prev_release"
 	echo "Genarating $PACKAGE_JSON_REL ..."
 	cat "$GITHUB_WORKSPACE/package/package_esp32_index.template.json" | jq "$jq_arg" > "$OUTPUT_DIR/$PACKAGE_JSON_REL"
 	if [ ! -z "$prev_release" ] && [ "$prev_release" != "null" ]; then
-		downloadAndMergePackageJSON "$prev_release/$PACKAGE_JSON_REL" "$OUTPUT_DIR/$PACKAGE_JSON_REL"
-		echo "Getting commits from $prev_release ..."
-		git -C "$GITHUB_WORKSPACE" log --oneline $prev_release.. > "$OUTPUT_DIR/commits.txt"
+		merge_package_json "$prev_release/$PACKAGE_JSON_REL" "$OUTPUT_DIR/$PACKAGE_JSON_REL"
 	fi
 	echo "Uploading $PACKAGE_JSON_REL ..."
 	echo "Download URL: "`git_safe_upload_asset "$OUTPUT_DIR/$PACKAGE_JSON_REL"`
@@ -83,5 +82,79 @@ fi
 
 echo "JSON definition file(s) created"
 echo
+
+if [ ! -z "$commits_release" ] && [ "$commits_release" != "null" ]; then
+	echo "Getting commits from $commits_release ..."
+	git -C "$GITHUB_WORKSPACE" log --oneline $commits_release.. > "$OUTPUT_DIR/commits.txt"
+fi
+
+
+#
+# Prepare Markdown release notes:
+#################################
+# - annotated tags only, lightweight tags just display message of referred commit
+# - tag's description conversion to relnotes:
+# 	first 3 lines (tagname, commiter, blank): ignored
+#	4th line: relnotes heading
+#	remaining lines: each converted to bullet-list item
+#	empty lines ignored
+#	if '* ' found as a first char pair, it's converted to '- ' to keep bulleting unified
+echo
+echo Preparing release notes
+echo -----------------------
+echo "Tag's message:"
+
+relNotesRaw=`git -C "$GITHUB_WORKSPACE" show -s --format=%b $RELEASE_TAG`
+readarray -t msgArray <<<"$relNotesRaw"
+arrLen=${#msgArray[@]}
+releaseNotes=""
+#process annotated tags only
+if [ $arrLen > 3 ] && [ "${msgArray[0]:0:3}" == "tag" ]; then 
+	ind=3
+	while [ $ind -lt $arrLen ]; do
+		if [ $ind -eq 3 ]; then
+			releaseNotes="#### ${msgArray[ind]}"
+			releaseNotes+=$'\r\n'
+		else
+			oneLine="$(echo -e "${msgArray[ind]}" | sed -e 's/^[[:space:]]*//')"
+			
+			if [ ${#oneLine} -gt 0 ]; then
+				if [ "${oneLine:0:2}" == "* " ]; then oneLine=$(echo ${oneLine/\*/-}); fi
+				if [ "${oneLine:0:2}" != "- " ]; then releaseNotes+="- "; fi		
+				releaseNotes+="$oneLine"
+				releaseNotes+=$'\r\n'
+				
+				#debug output
+				echo "   ${oneLine}"
+			fi
+		fi
+		let ind=$ind+1
+	done
+fi
+
+echo "$releaseNotes"
+
+# - list of commits (commits.txt must exit in the output dir)
+commitFile=$OUTPUT_DIR/commits.txt
+if [ -s "$commitFile" ]; then
+	
+	releaseNotes+=$'\r\n##### Commits\r\n'
+	
+	echo
+	echo "Commits:"
+		
+	IFS=$'\n'
+	for next in `cat $commitFile`
+	do
+		IFS=' ' read -r commitId commitMsg <<< "$next"
+		commitLine="- [$commitId](https://github.com/$varRepoSlug/commit/$commitId) $commitMsg"
+		echo "   $commitLine"
+		
+		releaseNotes+="$commitLine"
+		releaseNotes+=$'\r\n'
+	done
+	rm -f $commitFile
+fi
+
 
 set +e
