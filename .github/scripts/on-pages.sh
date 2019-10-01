07cffd01c1ddfc4fa747812eb73862baae625ec6
me-no-dev <hristo@espressif.com>
2019-10-01 13:30:04 +0300

    Update on-pages.sh

--- .github/scripts/on-pages.sh
+++ .github/scripts/on-pages.sh
@@ -6,12 +6,12 @@ EVENT_JSON=`cat $GITHUB_EVENT_PATH`
 echo "Event: $GITHUB_EVENT_NAME, Repo: $GITHUB_REPOSITORY, Path: $GITHUB_WORKSPACE, Ref: $GITHUB_REF"
 echo
 echo "Commits: "
-echo "  Added: "`echo "$EVENT_JSON" | jq -r '.commits.[].added'
-echo "  Modified: "`echo "$EVENT_JSON" | jq -r '.commits.[].modified'
-echo "  Removed: "`echo "$EVENT_JSON" | jq -r '.commits.[].removed'
+echo "  Added: "`echo "$EVENT_JSON" | jq -r '.commits[].added'`
+echo "  Modified: "`echo "$EVENT_JSON" | jq -r '.commits[].modified'`
+echo "  Removed: "`echo "$EVENT_JSON" | jq -r '.commits[].removed'`
 echo
 echo "Head Commit: "
-echo "  Added: "`echo "$EVENT_JSON" | jq -r '.head_commit.added'
-echo "  Modified: "`echo "$EVENT_JSON" | jq -r '.head_commit.modified'
-echo "  Removed: "`echo "$EVENT_JSON" | jq -r '.head_commit.removed'
+echo "  Added: "`echo "$EVENT_JSON" | jq -r '.head_commit.added'`
+echo "  Modified: "`echo "$EVENT_JSON" | jq -r '.head_commit.modified'`
+echo "  Removed: "`echo "$EVENT_JSON" | jq -r '.head_commit.removed'`
 echo
