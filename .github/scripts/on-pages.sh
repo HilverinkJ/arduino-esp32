b5a8084d6b987d3c3eab83fd61a9d506607d953a
me-no-dev <hristo@espressif.com>
2019-10-01 13:26:01 +0300

    Update on-pages.sh

--- .github/scripts/on-pages.sh
+++ .github/scripts/on-pages.sh
@@ -6,9 +6,9 @@ EVENT_JSON=`cat $GITHUB_EVENT_PATH`
 echo "Event: $GITHUB_EVENT_NAME, Repo: $GITHUB_REPOSITORY, Path: $GITHUB_WORKSPACE, Ref: $GITHUB_REF"
 echo
 echo "Commits: "
-echo "  Added: "`echo "$EVENT_JSON" | jq -r '.commits.added'
-echo "  Modified: "`echo "$EVENT_JSON" | jq -r '.commits.modified'
-echo "  Removed: "`echo "$EVENT_JSON" | jq -r '.commits.removed'
+echo "  Added: "`echo "$EVENT_JSON" | jq -r '.commits.[].added'
+echo "  Modified: "`echo "$EVENT_JSON" | jq -r '.commits.[].modified'
+echo "  Removed: "`echo "$EVENT_JSON" | jq -r '.commits.[].removed'
 echo
 echo "Head Commit: "
 echo "  Added: "`echo "$EVENT_JSON" | jq -r '.head_commit.added'
