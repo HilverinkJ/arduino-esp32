fc9e843346ac28768708b73bb65bc844c7c72ccc
me-no-dev <hristo@espressif.com>
2019-10-01 14:04:08 +0300

    Update on-pages.sh

--- .github/scripts/on-pages.sh
+++ .github/scripts/on-pages.sh
@@ -3,15 +3,38 @@
 
 EVENT_JSON=`cat $GITHUB_EVENT_PATH`
 
+pages_added=`echo "$EVENT_JSON" | jq -r '.commits[].added[]'`
+pages_modified=`echo "$EVENT_JSON" | jq -r '.commits[].modified[]'`
+pages_removed=`echo "$EVENT_JSON" | jq -r '.commits[].removed[]'`
+
 echo "Event: $GITHUB_EVENT_NAME, Repo: $GITHUB_REPOSITORY, Path: $GITHUB_WORKSPACE, Ref: $GITHUB_REF"
 echo
 echo "Commits: "
-echo "  Added: "`echo "$EVENT_JSON" | jq -r '.commits[].added'`
-echo "  Modified: "`echo "$EVENT_JSON" | jq -r '.commits[].modified'`
-echo "  Removed: "`echo "$EVENT_JSON" | jq -r '.commits[].removed'`
+echo "  Added: $pages_added"
+echo "  Modified: $pages_modified"
+echo "  Removed: $pages_removed"
 echo
-echo "Head Commit: "
-echo "  Added: "`echo "$EVENT_JSON" | jq -r '.head_commit.added'`
-echo "  Modified: "`echo "$EVENT_JSON" | jq -r '.head_commit.modified'`
-echo "  Removed: "`echo "$EVENT_JSON" | jq -r '.head_commit.removed'`
-echo
+
+for page in $pages_added; do
+	if [[ $page != "README.md" && $page != "docs/"* ]]; then
+		echo "Skipping '$page'"
+		continue
+	fi
+	echo "Adding '$page' to pages ..."
+done
+
+for page in $pages_modified; do
+	if [[ $page != "README.md" && $page != "docs/"* ]]; then
+		echo "Skipping '$page'"
+		continue
+	fi
+	echo "Modifying '$page' ..."
+done
+
+for page in $pages_removed; do
+	if [[ $page != "README.md" && $page != "docs/"* ]]; then
+		echo "Skipping '$page'"
+		continue
+	fi
+	echo "Removing '$page' from pages ..."
+done
